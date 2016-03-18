d42b7b340df45a8c1772f86466f2a7edc36d97c4
Diane Reta <diane.reta@gmail.com>
2016-03-18 11:35:26 +0800

    Added Rakefile and sequel migration

---
+++ 20160318_create_table.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+  up do
+    create_table(:users) do
+      primary_key :id
+    Boolean :admin
+    String :fname
+    String :lname
+    String :email
+    String :username
+    String :password_hash
+    String :password_salt
+    end
+  end
+
+  down do
+    drop_table(:users)
+  end
+end
