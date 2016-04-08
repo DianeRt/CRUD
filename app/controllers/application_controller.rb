b18f7109230a7c8670f6e6e1a676b9222cfe5d03
Diane Reta <diane.reta@gmail.com>
2016-04-09 02:19:38 +0800

    Added sprocket codes to application_controller and layout.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,28 @@ class ApplicationController < Sinatra::Base
   register Sinatra::Flash
   set :views, File.expand_path('../../views', __FILE__)
 
+
+  # Sprockets Setup
+  #-----------------------------------------------------------------
+  # initialize new sprockets environment
+  set :environment, Sprockets::Environment.new
+
+  # append assets paths
+  environment.append_path "assets/stylesheets"
+  environment.append_path "assets/javascripts"
+
+  # compress assets
+  # environment.js_compressor  = :uglify
+  # environment.css_compressor = :scss
+
+  # get assets
+  get "/assets/*" do
+    env["PATH_INFO"].sub!("/assets", "")
+    settings.environment.call(env)
+  end
+  #-----------------------------------------------------------------
+
+
   # create @users variable to give access to view files
   #-----------------------------------------------------------------
   def initialize
