9d8271db7be4284a75110b8edd8bf90da9020c55
Diane Reta <diane.reta@gmail.com>
2016-04-29 23:20:20 +0800

    Added validation when updating user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < Sequel::Model
   def validate
     super
     validates_presence [:fname, :lname, :email, :username]
-    validates_unique(:email, :username, [:fname, :lname])
-    validates_length_range 3..100, [:fname, :lname, :email, :username]
+    validates_min_length 3, [:fname, :lname, :email, :username]
+    validates_max_length 100, [:fname, :lname, :email, :username]
     validates_format /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :email
+    # validates_unique(:email, :username, [:fname, :lname]) # having trouble using this when updating info
   end
 
   def self.password(password=nil, salt=false)
