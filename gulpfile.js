0bc5597c052a745f8c85030436cd4333e2ff9789
Diane Reta <diane.reta@gmail.com>
2016-06-01 01:18:27 +0800

    Edited gulpfile to utilize browserify and babelify

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,18 @@ var concat = require('gulp-concat');
 var es = require('event-stream');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var fs = require('fs');
+var uglify = require('gulp-uglify');
 
-//variables to set source and destination paths
+//variables to set destination, source and node_module paths
 var destination = 'app/assets/dist';
 var source = 'app/assets/src';
+var node_modules = 'node_modules/bootstrap/dist';
 
 //using sass and yui compressor
+//using event-stream to create in-memory piece of data
 //this will compile, concatenate, minify all files in source
 gulp.task('css', function () {
   var compiledCSS =  gulp.src(source + '/**/*.{scss,sass}')
@@ -25,23 +31,36 @@ gulp.task('css', function () {
     .pipe(gulp.dest(destination + '/css'));
 });
 
-//using babel and yui compressor
+//using babel and gulp-uglify
 //this will concatenate, minify and add es6 js features to all files in source
 gulp.task('babel', function () {
-  return gulp.src(source + '/**/*.js')
+  return gulp.src([source + '/**/*.js', node_modules + '/js/bootstrap.min.js'])
     .pipe(sourcemaps.init())
     .pipe(concat('app.min.js'))
-    .pipe(babel({presets: ['es2015'] }))
-    .pipe(compress({type: 'js'}))
+    .pipe(babel({presets: ['es2015', 'react']}))
+    .pipe(uglify())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(destination + '/js'));
 });
 
+//using browserfiy and babelify
+//using fs to create bundle.js
+gulp.task('babelify', function () {
+  return browserify({
+      entries: [source + '/js'], //using source + '/js/*.js' creates events.js:154 throw er; //Unhandled 'error' event: Cannot find module
+      extensions: ['.js'],
+      debug: true})
+    .transform("babelify", {presets: ["es2015", "react"]})
+    .bundle()
+    .pipe(fs.createWriteStream(destination + '/js/bundle.js'))
+}); 
+
+
 // monitor files for changes
 gulp.task('watch', function() {
     gulp.watch(source + '/**/*.{css,scss,sass}', ['css']);
-    gulp.watch(source + '/**/*.js', ['babel']);
+    gulp.watch(source + '/**/*.js', ['babel', 'babelify']);
 });
 
 // Default Task
-gulp.task('default', ['css', 'babel', 'watch']);
+gulp.task('default', ['css', 'babel', 'babelify', 'watch']);
